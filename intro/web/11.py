48de4b35723cab6e57438f1f410a108b3b6f95a5
104matt <matteogiochi16@gmail.com>
2025-11-21 22:14:33 +0100

    Solved intro web 11 (OG)

--- intro/web/11.py
+++ intro/web/11.py
@@ -1,13 +1,14 @@
 import requests
-
+import json
 req = requests.session()
 body = {"username":"admin","password":"admin"}
 
-csrf = (req.post("http://web-11.challs.olicyber.it/login", json=body)).text
-r=req.post("http://web-11.challs.olicyber.it/login", json=body)
+csrf = json.loads(req.post("http://web-11.challs.olicyber.it/login", json=body).text)["csrf"]
 print(csrf)
-#print(r.json())
-#for i in range(4):
-    #f=req.get("http://web-11.challs.olicyber.it/flag_piece?index=", index=i)
+for i in range(4):
+    f= json.loads(req.get("http://web-11.challs.olicyber.it/flag_piece?index=" + str(i) + "&csrf=" + csrf).text)
+    csrf = f["csrf"]
+    print(f)
+
 
 
